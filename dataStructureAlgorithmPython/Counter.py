08bd01833de481a61e5d5ab540c1f80186d434c1
ZenithZyf <zenithzyf@gmail.com>
2019-11-25 17:23:23 -0600

    delete redundant codes

--- dataStructureAlgorithmPython/Counter.py
+++
@@ -1,6 +0,0 @@
-# Counter
-from collections import Counter
-
-a = [1,2,1,3,2,5,2,3,4]
-b = Counter(a) # b will be a dictionary
-# b = {1:2,2:3,3:2,4:1,5:1}
--- dataStructureAlgorithmPython/sorting.py
+++
@@ -1,8 +0,0 @@
-# sorting: 
-
-# selection sort: i-th iteration number in iteration i
-# number of computations: n^2/2-n/2; O(n^2)
-
-# merge sort: recursively divide and then merge
-
-# quick sort: first find a pivot element and then sort the list according to the pivot element
